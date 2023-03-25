96820d7b391097e1d2ec019cff942272518b5a40
noorkobari <noor.kobari9@gmail.com>
2023-03-25 15:02:09 +0300

    no message

--- src/app/pages/orders/payment/payment.component.ts
+++ src/app/pages/orders/payment/payment.component.ts
@@ -85,7 +85,14 @@ export class PaymentComponent implements OnInit {
           });
         }
       }
-      this.orderService.addOrder(this.order);
+      this.orderService.addOrder(this.order).subscribe(
+        (response) => {
+          console.log('Order created successfully', response);
+        },
+        (error) => {
+          console.log('Error creating order', error);
+        }
+      );
       this.orderService.add(this.order);
       this.dialogRef.close();
     }
