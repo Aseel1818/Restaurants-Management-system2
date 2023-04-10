7857bc80fba81602fdffec66a76e9a9827cfb205
noorkobari <noor.kobari9@gmail.com>
2023-04-10 18:25:24 +0300

    FormGroup

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
-import { FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  username = new FormControl('', Validators.required);
-  password = new FormControl('', [Validators.required, Validators.minLength(8),
-  Validators.pattern('^((?!.*[s])(?=.*[A-Z])(?=.*d).{8,99})')]);
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern('^((?!.*[s])(?=.*[A-Z])(?=.*d).{8,99})')
+    ])
+  });
+
   showErrorMessages = false;
 
   constructor(private authService: AuthService) {}
@@ -18,7 +25,7 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     let hasError = false;
-    if (this.username.invalid || this.password.invalid) {
+    if (this.loginForm.invalid) {
       hasError = true;
     }
     if (!hasError) {
@@ -29,8 +36,8 @@ export class LoginComponent implements OnInit {
 
   login() {
     this.authService.checkAuthStatus();
-    const username = this.username.value!;
-    const password = this.password.value!;
-    this.authService.login(username, password);
+    const username = this.loginForm.get('username')?.value;
+    const password = this.loginForm.get('password')?.value;
+    this.authService.login(username!, password!);
   }
 }
